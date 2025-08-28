3968ce5bcb43b4c5ceef8da504db14ef798f3c57
Zhenglinfang <zfan0035@student.monash.edu>
2025-08-29 05:08:16 +1000

    new frame try

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,1 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Swaps from '../pages/Swaps.vue'
-import Games from '../pages/Games.vue'
-import Avatar from '../pages/Avatar.vue'
-
-const routes = [
-  { path: '/', component: Home, name: 'home' },
-  { path: '/swaps', component: Swaps, name: 'swaps' },
-  { path: '/games', component: Games, name: 'games' },
-  { path: '/avatar', component: Avatar, name: 'avatar' },
-]
-
-export default createRouter({
-  history: createWebHistory(),
-  routes,
-  scrollBehavior() { return { top: 0 } }
-})
 
