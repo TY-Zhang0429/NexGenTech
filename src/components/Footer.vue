024f26ecc0663f6ecd5f37fdd2691561314b641b
ZTY <Zty000429@outlook.com>
2025-08-30 20:47:58 +1000

    Delete src directory

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="site">
-    <div class="container">© {{ year }} NexGenTech – UI prototype</div>
-  </footer>
-</template>
-
-<script setup>
-const year = new Date().getFullYear()
-</script>
