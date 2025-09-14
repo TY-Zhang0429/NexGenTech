e025759f504d29f8be3f580f7a0def3562092c26
tzha0142 <tzha0142student.monash.edu>
2025-09-15 09:28:56 +1000

    update deploy & router for archive release v1.0.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import WordleGameView from '@/views/WordleGameView.vue'
 import FoodSwapView from '@/views/FoodSwapView.vue'
 import GamePage from '@/views/GamePage.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomePage  },
-  { path: '/game', name: 'game', component: GamePage  },
-  { path: '/wordle-game', name: 'wordle-game', component: WordleGameView  },
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/game', name: 'game', component: GamePage },
+  { path: '/wordle-game', name: 'wordle-game', component: WordleGameView },
   { path: '/food-swap', name: 'food-swap', component: FoodSwapView },
 ]
 
+// checker if it's archive mode
+const isArchive = import.meta.env.VITE_ARCHIVE === '1'
+
+const history = isArchive
+  ? createWebHashHistory()                // archive uses hash mode to avoid 404
+  : createWebHistory(import.meta.env.BASE_URL) // main site uses history mode
+
 export default createRouter({
-  history: createWebHistory(),
+  history,
   routes,
   scrollBehavior: () => ({ top: 0 }),
 })
